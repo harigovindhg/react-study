9c8fdca8b198fbfcf23ba75fdb5e5d4096c4a152
harigovindhg <hgharigovind90@gmail.com>
2024-02-14 22:40:08 +0530

    corrected data path

--- src/Work/components/Body.js
+++ src/Work/components/Body.js
@@ -24,8 +24,8 @@ const Body = () => {
         try {
             const data = await fetch("https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D13.0529437%26lng%3D77.6315163%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING");
             const json = await data.json();
-            setRestaurantListLocal(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);     // Optional Chaining for nullish checks
-            setSearchResults(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);     // Optional Chaining for nullish checks
+            setRestaurantListLocal(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);     // Optional Chaining for nullish checks
+            setSearchResults(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);     // Optional Chaining for nullish checks
             setDataLoaded(true);
         } catch (error) {
             console.log('data fetch failed');
